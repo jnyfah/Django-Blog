cc14aa2a97c97da81c47b4d98ac5268a33ea9d5e
jnyfah <jnyfaah@gmail.com>
2019-03-17 00:36:13 +0100

    template

--- blog/pages/views.py
+++ blog/pages/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+posts = [
+  {
+    'author': 'jenny',
+    'title': 'Blog post 1',
+    'content':'first post content',
+    'date_posted': 'march 16,2019'
+  },
+  
+  {'author': 'Emma',
+    'title': 'Blog post 2',
+    'content':'second post content',
+    'date-posted': 'march 17,2019'
+  }
+]
+
+
+
+def home(request):
+  context = {
+    'posts': posts
+  }
+  return render(request, 'home.html',context)
+
+def about(request):
+  return render(request, 'about.html')
