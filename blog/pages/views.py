1744ce7a0c7a3f70d74eae316ecedfa6912d11e1
jnyfah <jnyfaah@gmail.com>
2019-03-19 12:39:49 +0100

    user registration

--- blog/pages/views.py
+++ blog/pages/views.py
@@ -1,29 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
 
 # Create your views here.
-
-
-posts = [
-  {
-    'author': 'jenny',
-    'title': 'Blog post 1',
-    'content':'first post content',
-    'date_posted': 'march 16,2019'
-  },
-  
-  {'author': 'Emma',
-    'title': 'Blog post 2',
-    'content':'second post content',
-    'date-posted': 'march 17,2019'
-  }
-]
-
-
-
 def home(request):
   context = {
-    'posts': posts
+    'posts': Post.objects.all()
   }
   return render(request, 'home.html',context)
 
